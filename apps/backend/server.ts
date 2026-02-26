24a2f6f9f315db7685bc1d7eb0b99c5efe7681dc
SimzMapisa <simbarashe795@gmail.com>
2026-02-26 03:14:03 +0000

    Enhance server setup by adding cookie and JWT support, and update auth route registration

--- apps/backend/server.ts
+++ apps/backend/server.ts
@@ -1,17 +1,28 @@
+import cookie from '@fastify/cookie';
+import jwt from '@fastify/jwt';
 import 'dotenv/config';
 import Fastify from 'fastify';
-import { registerRoutes } from './routes/auth/register.js';
-
+import { authRoutes } from './src/modules/auth/auth.routes';
 
 export const app = Fastify({
 	logger: true,
 });
 
+
+
+app.register(cookie, {
+  secret: process.env.COOKIE_SECRET!, 
+})
+
+app.register(jwt, {
+  secret: process.env.JWT_SECRET!,
+})
+
 app.get('/health', async () => {
 	return { status: 'okay', timestamp: new Date().toISOString() };
 });
 
-app.register(registerRoutes, { prefix: '/auth' });
+app.register(authRoutes, { prefix: '/auth' });
 
 // start the server
 const start = async () => {
