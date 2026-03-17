464b6533a12c7c5c1c8d00bffc8e7fc07bb4ae82
SimzMapisa <simbarashe795@gmail.com>
2026-03-17 18:01:57 +0000

    Revert "feat: authentication"
    This reverts commit 779bdf20629908b505f660699827b317d7221c59.
    
    move changes to the correct feature branch

--- apps/backend/src/modules/auth/auth.routes.ts
+++ apps/backend/src/modules/auth/auth.routes.ts
@@ -1,41 +1,9 @@
-import type { FastifyInstance } from 'fastify';
-import {
-	acceptInviteController,
-	forgotPasswordController,
-	loginController,
-	logoutController,
-	refreshController,
-	registerController,
-	changePasswordController,
-} from './auth.controller';
-import {
-	acceptInviteOpts,
-	changePasswordOpts,
-	forgotPasswordOpts,
-	loginOpts,
-	registerOpts,
-	resetPasswordOpts,
-} from './auth.schemas';
-import { authenticate } from '../../middleware/authenticate';
+import type { FastifyInstance } from "fastify";
+import { loginController, logoutController, registerController } from "./auth.controller";
+import { loginOpts, registerOpts } from "./auth.schemas";
 
 export async function authRoutes(app: FastifyInstance) {
-	app.post('/register', registerOpts, registerController(app));
-	app.post('/login', loginOpts, loginController(app));
-	app.delete(
-		'/logout',
-		{ preHandler: [authenticate(app)] },
-		logoutController(app),
-	);
-	app.post(
-		'/forgot-password',
-		forgotPasswordOpts,
-		forgotPasswordController(app),
-	);
-	app.put(
-		'/change-password',
-		{ ...changePasswordOpts, preHandler: [authenticate(app)] },
-		changePasswordController(app),
-	);
-	app.post('/accept-invite', acceptInviteOpts, acceptInviteController(app));
-	app.post('/refresh', refreshController(app));
+    app.post("/register", registerOpts, registerController(app));
+    app.post("/login", loginOpts, loginController(app));
+    app.delete("/logout", logoutController(app));
 }
